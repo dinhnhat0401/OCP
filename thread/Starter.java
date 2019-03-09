97f35a8b8c05d8d28b337c83973c207bb0b5c76d
dinhnhat0401 <nhatphutho@gmail.com>
2019-03-09 19:22:40 +0900

    Sample about calling run() method directly on a class implements Runnable

---
+++ thread/Starter.java
@@ -0,0 +1,15 @@
+public class Starter implements Runnable {
+  void go(long id) {
+    System.out.println(id);
+  }
+
+  public static void main(String[] args) {
+    System.out.println(Thread.currentThread().getId() + " ");
+    //new Starter().run();
+    //new Thread(new Starter()).run();
+    new Thread(new Starter()).start();
+  }
+
+  public void run() { go(Thread.currentThread().getId()); }
+}
+
