bb31360b54e76d7318aba8c61fd5b1b79c6e5950
dinhnhat0401 <nhatphutho@gmail.com>
2019-03-17 23:31:14 +0900

    Thread Executor

---
+++ Concurrency/CB.java
@@ -0,0 +1,4 @@
+public class CB {
+  List<String> result = new ArrayList
+}
+
---
+++ Concurrency/SameThreadExecutor.java
@@ -0,0 +1,9 @@
+import java.util.concurrent.Executor;
+
+public class SameThreadExecutor implements Executor {
+  @Override
+  public void execute(Runnable command) {
+    command.run();
+  }
+}
+
