68fe8231bcbfe37f3eeb7d21fc49a6ac63b0304f
Nhat Dinh <nhat.dinh@rakuten.com>
2019-05-23 21:28:47 +0900

    A sub class's method need stronger access privilege to not break  liskov substitution

--- class design/Hammer.java
+++ class design/Hammer.java
@@ -5,6 +5,7 @@ class Tool {
 class Hammer extends Tool {
   private void use() { } // This will caused `attempting to assign weaker access privileges; was package` error
   private void use(String s) { } // This will work normally, is a trick in OCP example. Overloaded method is not restricted by the parent class access privileges.
+  public void use() { } // This will work normally because we are assigning stronger access privileges.
   public void bang() { }
 }
 
