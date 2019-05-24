d02ba14aed06c0464af57e278e8dc7d395760eb2
Nhat Dinh <nhat.dinh@rakuten.com>
2019-05-25 00:05:54 +0900

    play with nested class, innner class

---
+++ class design/Outer.java
@@ -0,0 +1,31 @@
+public class Outer {
+  class Inner {
+    
+  }
+
+  static class StaticInner {
+    
+  }
+
+  public static void main(String... args) {
+    class MethodInner {
+    }
+
+    // This is illegal, only nested class can be static    
+    //static class StaticMethodInner {
+    //}
+    
+    Outer o = new Outer();
+    
+    // Because Inner is not a static class, we need to initialize an Outer object
+    // then use that object to initialize an inner class
+    Inner inner = o.new Inner();
+
+    // StaticInner is a static class
+    StaticInner staticInner = new Outer.StaticInner();
+
+    // instantiate a new local class 
+    MethodInner methodInner = new MethodInner();
+  } 
+}
+
