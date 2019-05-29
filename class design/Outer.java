187587f433162389809862731a7055a85a9cbfb1
Nhat Dinh <nhat.dinh@rakuten.com>
2019-05-29 23:18:14 +0900

    inside the outer class's method, we can call inner class with or without outer class name

--- class design/Outer.java
+++ class design/Outer.java
@@ -19,10 +19,12 @@ public class Outer {
     
     // Because Inner is not a static class, we need to initialize an Outer object
     // then use that object to initialize an inner class
+    Inner inner1 = o.new Inner();
     Outer.Inner inner = o.new Inner();
 
     // StaticInner is a static class
     StaticInner staticInner = new Outer.StaticInner();
+    Outer.StaticInner staticInner2 = new Outer.StaticInner();
 
     // instantiate a new local class 
     MethodInner methodInner = new MethodInner();
