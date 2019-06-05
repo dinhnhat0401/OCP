b5da69a009a4232c76634e08753109256c321a06
dinhnhat0401 <nhatphutho@gmail.com>
2019-06-05 23:26:45 +0900

    Incomparable type error will be returned if we compare two different type of enum

---
+++ class design/CompareTwoEnums.java
@@ -0,0 +1,18 @@
+public class CompareTwoEnums {
+    enum JobStatus {
+        SUCCESS, FAIL; //Line 3
+    }
+
+    enum TestResult {
+        PASS, FAIL; //Line 7
+    }
+
+    public static void main(String[] args) {
+        JobStatus js = JobStatus.FAIL;
+        TestResult tr = TestResult.FAIL;
+
+        System.out.println(js.equals(tr)); //Line 14
+        System.out.println(js == tr); //Line 15
+    }
+}
+
