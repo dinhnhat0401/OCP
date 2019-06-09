8ecc4e869743ac65eb15cefe777ae6916a4e7a8e
Nhat Dinh <nhat.dinh@rakuten.com>
2019-06-09 11:05:38 +0900

    ClassCastException is thrown because of trying to cast RuntimeException instance to sub-type ArithmeticException

---
+++ exception/TypecastLeadToException.java
@@ -0,0 +1,22 @@
+public class TypecastLeadToException {
+  public static void main(String[] args) {
+    try {
+      check();
+    } catch(RuntimeException e) {
+      System.out.println(e.getClass().getName());
+    }
+  }
+
+  private static void check() {
+    try {
+      RuntimeException re = new RuntimeException();
+      throw re;
+    } catch(RuntimeException e) {
+      System.out.println(1);
+      ArithmeticException ex = (ArithmeticException)e;
+      System.out.println(2);
+      throw ex;
+    }
+  }
+}
+
